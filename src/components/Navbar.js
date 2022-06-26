5af0ecd3a38eb7fa67adccc2c1bb6eef7fee6f27
DaNiels89 <nielssloesen@hotmail.com>
2022-06-26 12:15:43 +0200

    ADJUSTED styles and semantic HTML

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <header>
-      <nav>
-        Navbar
+      <nav class="primary-menu">
+        <Link to="/">FitnessApp</Link>
+        <ul class="primary-menu__list">
+          <li class="primary-menu__list-item"><Link to="/">Home</Link></li>
+          <li class="primary-menu__list-item"><Link to="/excercises/">Excercises</Link></li>
+        </ul>
       </nav>
     </header>
   )
